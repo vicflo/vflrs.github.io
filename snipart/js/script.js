86431b5d7351b66dec8f8fc9e900bd8cb02c7a71
thomhersbach <31585963+thomhersbach@users.noreply.github.com>
2018-05-28 08:22:14 +0200

    MMEKies

--- snipart/js/script.js
+++ snipart/js/script.js
@@ -33,6 +33,10 @@
   //   }
   // });     
 
+  
+
+  var noHash = new RegExp('#([^\\s]*)','g');
+
   $.ajax({
     url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent',
     dataType: 'jsonp',
@@ -40,9 +44,55 @@
     data: {access_token: token, count: num_photos},
     success: function(data){
       console.log(data);
-      for(x in data.data){
-        $('ul').append('<li><img src="'+data.data[x].images.standard_resolution.url+'"></li>');  
-      }
+      var x = 0;
+      $(".content").each(function() {
+
+       $(this).append($('<div>', {class: 'bg'}));
+        var big = $(this).children('.bg');
+
+        $(big).css('background-image', 'url(' + data.data[x].images.standard_resolution.url + ')');
+
+        // console.log(data.data[x].caption.text);
+        // Getting Image TAG, parsing and creating node
+        var text = data.data[x].caption.text;
+        var postText = text.replace(noHash, '');
+        var tidbit = document.createElement('h3');
+        txTidbit = document.createTextNode(postText);
+        tidbit.appendChild(txTidbit);
+        // Getting Image LIKES and creating node
+        var liked = data.data[x].likes.count;
+        var hearts = document.createElement('h2');
+        txt = document.createTextNode('❤ ' + liked.toString());
+        hearts.appendChild(txt);
+        // Getting Image LINK and creating link
+        var imgLink = data.data[x].link;
+        var toLink = $('<a>',{
+          // text: 'view on instagram',
+          target: '_blank',
+          class: 'button',
+          href: imgLink,
+        });
+
+        toLink.append(hearts);
+
+
+        // console.log("-- " + postText);
+        var child = $(this).children('.caption');
+        // child.append(postText);
+        // console.log("likes " + liked.toString());
+        // $(this).append('❤ ' + liked.toString());
+        // var imageLikes = '❤ ' + liked.toString();
+
+        // child.append(hearts);
+        child.append(toLink);
+        child.append(tidbit);
+  
+      
+      // child.append(' ❤ ' + liked.toString());
+
+        x++;
+      });
+
     },
     error: function(data){
       console.log(data);
